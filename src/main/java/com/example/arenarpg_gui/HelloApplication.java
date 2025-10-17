c01904a400b5f3d1083eb845a1c09b43564b9720
Giovanni <giocheris63@gmail.com>
2025-10-17 13:46:16 -0400

    Added Icon

--- src/main/java/com/example/arenarpg_gui/HelloApplication.java
+++ src/main/java/com/example/arenarpg_gui/HelloApplication.java
@@ -3,9 +3,12 @@ package com.example.arenarpg_gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -13,6 +16,14 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Capstone.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("ArenaRPG");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Icon/ArenaRPG.png"))));
+        try{
+            Taskbar taskbar = Taskbar.getTaskbar();
+            java.awt.Image dockIcon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Icon/ArenaRPG.png"));
+            taskbar.setIconImage(dockIcon);
+        } catch (Exception e) {
+            System.out.println("Icon not found" + e.getMessage());
+        }
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,8 @@ module com.example.arenarpg_gui {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires javafx.graphics;
+    requires java.desktop;
 
     opens com.example.arenarpg_gui to javafx.fxml;
     exports com.example.arenarpg_gui;
