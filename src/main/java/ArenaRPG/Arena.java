7974a2249b537f5ff8b844b9d92f24bbbd3a26db
Giovanni <giocheris63@gmail.com>
2025-10-28 09:34:15 -0400

    Major changes to game logic

--- src/main/java/ArenaRPG/Arena.java
+++ src/main/java/ArenaRPG/Arena.java
@@ -102,56 +102,56 @@ public class Arena {
         }
     }
 
-    public void oldBattleMove() {
-
-
-        System.out.println("Your stats: " +
-                "\nBase Power: " + fighter.baseStrength +
-                ", Base Endurance: " + fighter.baseDefense +
-                ", Battle Power: " + fighter.playerPower() +
-                ", Equipped Weapon: " + (fighter.getWeapon() != null ? fighter.getWeapon().weaponName : "None"));
-        System.out.println("Enemy's stats: " +
-                "\nBase Power: " + opponent.baseStrength +
-                ", Base Endurance: " + opponent.baseDefense +
-                ", Battle Power: " + opponent.enemyPower());
-
-        System.out.println("Player Turn.");
-        System.out.println("Your opponent Stands before you, what will you do?\n(on your keyboard type: z to attack, x to defend, or c to forfeit)");
-        String action = input.nextLine();
-
-        if (action.equals("z")) {
-            // Player attacks
-            int playerBase = fighter.playerPower();
-            int playerMod = applyHitModifiers(playerBase, "you");
-            opponent.takeHit(playerMod);
-
-            // Enemy retaliates if alive
-            if (opponent.isAlive()) {
-                System.out.println("Enemy Turn");
-                int enemyBase = opponent.enemyPower();
-                int enemyMod = applyHitModifiers(enemyBase, "the enemy");
-                fighter.takeHit(enemyMod); // updated signature
-            }
-
-        } else if (action.equals("x")) {
-            // Player defends; enemy attacks with reduced effect
-            int enemyBase = opponent.enemyPower();
-            int enemyMod = applyHitModifiers(enemyBase, "the enemy");
-            fighter.reducedHit(enemyMod); // updated signature
-
-        } else if (action.equals("c")) {
-            System.out.println("There is always next time, I suppose...\n(if you wish to play again, please rerun the code.)");
-            return;
-
-        } else {
-            System.out.println("Invalid input. Please choose z, x, or c.");
-        }
-
-        if (fighter.health <= 0) {
-            System.out.println("You have fallen in battle and lost...\n(if you wish to play again, please rerun the code.)");
-        }
-        if (opponent.health <= 0) {
-            System.out.println("You have bested your opponent and won!!\n(if you wish to play again, please rerun the code.)");
-        }
-    }
+//    public void oldBattleMove() {
+//
+//
+//        System.out.println("Your stats: " +
+//                "\nBase Power: " + fighter.baseStrength +
+//                ", Base Endurance: " + fighter.baseDefense +
+//                ", Battle Power: " + fighter.playerPower() +
+//                ", Equipped Weapon: " + (fighter.getWeapon() != null ? fighter.getWeapon().weaponName : "None"));
+//        System.out.println("Enemy's stats: " +
+//                "\nBase Power: " + opponent.baseStrength +
+//                ", Base Endurance: " + opponent.baseDefense +
+//                ", Battle Power: " + opponent.enemyPower());
+//
+//        System.out.println("Player Turn.");
+//        System.out.println("Your opponent Stands before you, what will you do?\n(on your keyboard type: z to attack, x to defend, or c to forfeit)");
+//        String action = input.nextLine();
+//
+//        if (action.equals("z")) {
+//            // Player attacks
+//            int playerBase = fighter.playerPower();
+//            int playerMod = applyHitModifiers(playerBase, "you");
+//            opponent.takeHit(playerMod);
+//
+//            // Enemy retaliates if alive
+//            if (opponent.isAlive()) {
+//                System.out.println("Enemy Turn");
+//                int enemyBase = opponent.enemyPower();
+//                int enemyMod = applyHitModifiers(enemyBase, "the enemy");
+//                fighter.takeHit(enemyMod); // updated signature
+//            }
+//
+//        } else if (action.equals("x")) {
+//            // Player defends; enemy attacks with reduced effect
+//            int enemyBase = opponent.enemyPower();
+//            int enemyMod = applyHitModifiers(enemyBase, "the enemy");
+//            fighter.reducedHit(enemyMod); // updated signature
+//
+//        } else if (action.equals("c")) {
+//            System.out.println("There is always next time, I suppose...\n(if you wish to play again, please rerun the code.)");
+//            return;
+//
+//        } else {
+//            System.out.println("Invalid input. Please choose z, x, or c.");
+//        }
+//
+//        if (fighter.health <= 0) {
+//            System.out.println("You have fallen in battle and lost...\n(if you wish to play again, please rerun the code.)");
+//        }
+//        if (opponent.health <= 0) {
+//            System.out.println("You have bested your opponent and won!!\n(if you wish to play again, please rerun the code.)");
+//        }
+//    }
 }
